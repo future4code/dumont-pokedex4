b1cb6e027ce487414bd702d52b3b8cf808fd56a4
Nicole <nzolnier@gmail.com>
2020-11-24 16:21:47 -0300

    add card pegando informações da api

--- projeto-pokedex/src/pages/HomePage.js
+++ projeto-pokedex/src/pages/HomePage.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Header from '../components/MainHome/Header'
 import Footer from '../components/Footer/Footer'
+import PokemonContext from '../contexts/PokemonContext'
+import { useRequestData } from '../hooks/useRequestData'
+import { baseUrl } from '../constants/urls'
+import CardPokemon from '../components/MainHome/CardPokemon'
+import styled from 'styled-components'
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  gap: 1em;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  padding: 2em;
+  padding-left: 4em;
+`
 
 function HomePage() {
-    return (
-      <div>
-          <Header />
-        <p>HomePage</p>
-        <Footer />
-      </div>
-    );
-  }
-  
-  export default HomePage;
-  
+  const data = useRequestData(`${baseUrl}`, undefined)
+
+  return (
+    <div>
+      <Header />
+      <GridContainer>
+      {data && data.results.map((item) => {
+        return <CardPokemon url={item.url} />
+      })}
+      </GridContainer>
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
