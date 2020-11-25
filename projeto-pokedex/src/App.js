ad6620dd8ce21e78a16446a98ab14c6675e7cec0
Nicole <nzolnier@gmail.com>
2020-11-25 19:11:56 -0300

    funcao dos cards feita com o globalstate

--- projeto-pokedex/src/App.js
+++ projeto-pokedex/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react'
+import GlobalState from './global/GlobalState'
 import Router from './router/Router'
-import PokemonContext from './contexts/PokemonContext'
-
-const pokemons = {}
 
 function App() {
 
   return (
-    <PokemonContext.Provider value={pokemons}>
-      <div className="App">
+      <GlobalState>
         <Router />
-      </div>
-    </PokemonContext.Provider>
+      </GlobalState>
   );
 }
 
 export default App;
+
+// app limpinho, apenas com o globalState e o router, como a chijo ensinou na aula <3
---
+++ projeto-pokedex/src/global/GlobalStateContext.js.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GlobalStateContext = React.createContext();
+
+export default GlobalStateContext;
+
+// criando aquele context massa
