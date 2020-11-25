f90766f1a6446c6f3530dfbde356e5aaaa6a4a7d
Luciana Verdinelli <luverdinelli@gmail.com>
2020-11-25 17:25:09 -0300

    Parte da Pagina de Detalhes que a Ruth fez

---
+++ projeto-pokedex/src/components/MainDetails/styled.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+export const BoxA = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+text-align: center;
+align-content: center;
+`
+export const PokeImgContainer = styled.div`
+background-color: #f2f2f2f2;
+height: 230px;
+width: 230px;
+margin: 20px;
+box-shadow: 3px 3px 3px 3px #d9d9d9;
+display: flex;
+justify-content: center;
+text-align: center;
+align-items: center;
+`
+export const BoxB = styled.div`
+display: flex;
+justify-content: column;
+text-align: center;
+`
+export const PokeStatsContainer = styled.div`
+background-color: #f2f2f2f2;
+height: 540px;
+width: 290px;
+margin: 20px;
+box-shadow: 3px 3px 3px 3px #d9d9d9;
+display: flex;
+flex-direction: column;
+justify-content:center;
+text-align: center;
+align-items: center;
+`
+export const BoxC = styled.div`
+display:flex;
+flex-direction: column;
+`
+export const PokeMovesContainer = styled.div`
+background-color: #f2f2f2f2;
+height: 439px;
+width: 300px;
+margin: 20px;
+box-shadow: 3px 3px 3px 3px #d9d9d9;
+display: flex;
+flex-direction: column;
+justify-content:center;
+text-align: center;
+align-items: center;
+` 
+export const DetailsFlexBox = styled.main`
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+padding-top: 8%;
+`
--- projeto-pokedex/src/pages/DetailsPage.js
+++ projeto-pokedex/src/pages/DetailsPage.js
@@ -1,12 +1,26 @@
-import React from 'react'
-
-function DetailsPage() {
-    return (
-      <div>
-        <p>DetailsPage</p>
-      </div>
-    );
-  }
-  
-  export default DetailsPage;
+import React from 'react';
+import Img from '../components/MainDetails/Img';
+import PokemonCard from '../components/MainDetails/PokemonCard';
+import PokemonAttack from '../components/MainDetails/PokemonAttack';
+import {BoxA, BoxB, BoxC, DetailsFlexBox} from '../components/MainDetails/styled';
+const DetailsPage = () => {
+    return(
+        <div>
+            <h1>Pokemons Details</h1>
+            <DetailsFlexBox>
+            <BoxA>
+            <Img/>
+            <Img/>
+            </BoxA>
+            <BoxB>
+            <PokemonAttack/>           
+            </BoxB>
+            <BoxC>            
+            <PokemonCard/>
+            </BoxC>
+            </DetailsFlexBox>
+        </div>
+    )
+}
+export default DetailsPage;
   
